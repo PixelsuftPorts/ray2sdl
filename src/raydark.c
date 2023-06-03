bf631be90a1937bc40ae072cdad5a33b5f193230
Pixelsuft <yuskivl07@mail.ru>
2023-06-03 14:42:06 +0700

    upd

--- src/raydark.c
+++ src/raydark.c
@@ -1,20 +1,51 @@
-#ifdef _WIN32
 #include <SDL2/SDL_syswm.h>
+#ifdef SDL_VIDEO_DRIVER_WINDOWS
 #include <windows.h>
 #include <stdbool.h>
 #endif
 
 void* GetHandleBySDLWindow(SDL_Window* window) {
-#ifdef _WIN32
     SDL_SysWMinfo wm_info;
     SDL_VERSION(&wm_info.version);
     SDL_GetWindowWMInfo(window, &wm_info);
+#ifdef SDL_VIDEO_DRIVER_WINDOWS
     return (void*)wm_info.info.win.window;
 #endif
+#ifdef SDL_VIDEO_DRIVER_WINRT
+    return (void*)wm_info.info.winrt.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_X11
+    return (void*)wm_info.info.x11.window; // Right?
+#endif
+#ifdef SDL_VIDEO_DRIVER_WAYLAND
+    return (void*)wm_info.info.wl.egl_window; // Right?
+#endif
+#ifdef SDL_VIDEO_DRIVER_DIRECTFB
+    return (void*)wm_info.info.directfb.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_COCOA
+    return (void*)wm_info.info.cocoa.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_UIKIT
+    return (void*)wm_info.info.uikit.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_ANDROID
+    return (void*)wm_info.info.android.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_OS2
+    return (void*)wm_info.info.os2.hwnd;
+#endif
+#ifdef SDL_VIDEO_DRIVER_VIVANTE
+    return (void*)wm_info.info.vivante.window;
+#endif
+#ifdef SDL_VIDEO_DRIVER_KMSDRM
+    return (void*)wm_info.info.kmsdrm.gbm_dev;
+#endif
+    return NULL;
 }
 
 void CheckDarkMode(SDL_Window* window) {
-#ifdef _WIN32
+#ifdef SDL_VIDEO_DRIVER_WINDOWS
     HMODULE dwm = LoadLibraryA("dwmapi.dll");
     if (!dwm)
         return;
