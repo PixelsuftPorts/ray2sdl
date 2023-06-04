b18b00ee6b880ad543cfae9f5aee008ae3eb80cc
Pixelsuft <yuskivl07@mail.ru>
2023-06-04 18:11:34 +0700

    upd

--- test1.c
+++ test1.c
@@ -36,7 +36,7 @@ int main(void)
         }
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawCircle(GetMouseX(), GetMouseY(), 10.0f, RED);
+        DrawCircle(GetMouseX(), GetMouseY(), 25.0f, RED);
             //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
